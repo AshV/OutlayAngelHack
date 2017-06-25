44dc2c3ffecf777e51da5276373ea2844432d5c8
AshV <ashish6318@gmail.com>
2017-06-25 13:33:58 +0530

    Basic presentation ready.

--- OutlayElf.js
+++ OutlayElf.js
@@ -1,22 +1,25 @@
 function MessageHandler(context, event) {
-    context.simplehttp.makePost("https://outlaystore.firebaseio.com/.json", '[{"time":"11:13:55 PM","milliseconds_since_epoch":1498346035806,"date":"06-24-2017"},{"money":20.5},{"Purpose":"Mango"}]', { "Content-Type": "application/json" });
-
     var msg = event.message;
     var lMsg = msg.toLowerCase();
+    context.console.log('0');
     if (IsNumber(lMsg[0])) {
+        context.console.log('1');
         var expense = IsValidMoney(msg);
-        if (!expense) {
+        if (true) {
+            context.console.log('2');
             AddExpense(context, expense);
         }
     }
     else if (msg[0] == '-') {
+        context.console.log('3');
         var gain = IsValidMoney(msg);
-        if (!gain) {
+        if (true) {
+            context.console.log('4');
             AddGain(context, gain);
         }
     }
-    else if (lMsg == 'day' || lMsg == 'week') {
-        HandleReport(lMsg);
+    else if (lMsg == 'day' || lMsg == 'help') {
+        HandleReport(context, lMsg);
     }
     else {
         HandleJunk(context);
@@ -44,23 +47,35 @@ function IsValidMoney(money) {
 }
 
 // Handle report related keywords
-function HandleReport(context) {
-    //   context.simplehttp.makeGet('http://date.jsontest.com/')
+function HandleReport(context, msg) {
+    if (msg == 'day') {
+        context.console.log('5');
+        context.simplehttp.makeGet("https://outlaystore.firebaseio.com/.json");
+    }
+    else if (msg == 'help') {
+        context.console.log('05');
+        context.sendResponse('Send your-amount space purpose to add Expense.<br/> Ex. 100 Cab <br/><br/> Send minus symbol your amount space recievedfrom to add Recieved Money. <br/>Ex. -500 Lucky Draw. <br/><br/>Send day for summary');
+    }
 }
 
 // Handle Junk
 function HandleJunk(context) {
-    context.sendResponse("Command not supported.");
+    context.sendResponse("Command not supported. send help for help.");
 }
 
 // Adding gain to Cash in Hand
 function AddGain(context, gain) {
-    context.simplehttp.makePost("https://outlaystore.firebaseio.com/.json", '[{"time":"11:13:55 PM","milliseconds_since_epoch":1498346035806,"date":"06-24-2017"},{"money":20.5},{"Purpose":"Mango"}]', null);
+    //  context.sendResponse('[{"date":"06-25-2017"},{"money":"'+gain[0]+'"},{"Purpose":"'+gain[1]+'"}]');
+    context.simplehttp.makePost("https://outlaystore.firebaseio.com/.json",
+        '[{"date":"06-25-2017"},{"money":"' + gain[0] + '"},{"Purpose":"' + gain[1] + '"}]',
+        { "Content-Type": "application/json" });
 }
 
 // Adding new expense Cash in Hand
 function AddExpense(context, expense) {
-
+    context.simplehttp.makePost("https://outlaystore.firebaseio.com/.json",
+        '[{"date":"06-25-2017"},{"money":"' + expense[0] + '"},{"Purpose":"' + expense[1] + '"}]',
+        { "Content-Type": "application/json" });
 }
 
 // Cash In Hand Management
@@ -88,8 +103,9 @@ function EventHandler(context, event) {
 
 function HttpResponseHandler(context, event) {
     //    if (event.geturl === httpip - api.comjson)
+    context.console.log('6');
     context.sendResponse(event.getresp);
-    context.sendResponse(event.params);
+    //   context.sendResponse(event.params);
 }
 
 function DbGetHandler(context, event) {
