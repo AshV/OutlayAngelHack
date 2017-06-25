cd5736d4de67ef93e7cdb9ceca4f9183f5fb03cb
AshV <ashish6318@gmail.com>
2017-06-25 06:59:50 +0530

    some meaningful code now ;)

--- OutlayElf.js
+++ OutlayElf.js
@@ -1,36 +1,63 @@
 function MessageHandler(context, event) {
-    context.console.log(test)
-    if(event.message.toLowerCase() == httptest) {
-        context.simplehttp.makeGet(httpip-api.comjson);
+    var msg = event.message;
+    var lMsg = msg.toLowerCase();
+    if (IsNumber(lMsg[0])) {
+        var expense = IsValidMoney(msg);
+        if (!expense) {
+            AddExpense(context, expense);
+        }
     }
-    else if(event.message.toLowerCase() == testdbget) {
-        context.simpledb.doGet(putby)
+    else if (msg[0] == '-') {
+        var gain = IsValidMoney(msg);
+        if (!gain) {
+            AddGain(context, gain);
+        }
     }
-    else if(event.message.toLowerCase() == testdbput) {
-        context.simpledb.doPut(putby, event.sender);
+    else if (lMsg == 'day' || lMsg == 'week') {
+        HandleReport(lMsg);
     }
     else {
-        context.sendResponse('No keyword found  '+event.message); 
+        HandleJunk(context);
     }
+}
+
+// Check if Character is Number
+function IsNumber(char) {
+    var isNum = false;
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].forEach(function (n) {
+        if (char == n)
+            isNum = true;
+    });
+    return isNum;
+}
+
+// Whether amount given is valid
+function IsValidMoney(money) {
+    var firstSpace = money.indexOf(" ");
+    var numPart = parseFloat(money.substr(0, firstSpace));
+    if (numPart == NaN)
+        return false;
+    else
+        return [numPart, money.substr(firstSpace+1)];
 }
 
 // Handle report related keywords
-function HandleReport(command) {
-
+function HandleReport(context) {
+ //   context.simplehttp.makeGet('http://date.jsontest.com/')
 }
 
 // Handle Junk
-function HandleJunk(junk) {
-
+function HandleJunk(context) {
+    context.sendResponse("Command not supported.");
 }
 
 // Adding gain to Cash in Hand
-function AddGain(gain) {
-
+function AddGain(context, gain) {
+    context.simplehttp.makePost("https://outlaystore.firebaseio.com/.json", '[{"time":"11:13:55 PM","milliseconds_since_epoch":1498346035806,"date":"06-24-2017"},{"money":20.5},{"Purpose":"Mango"}]', null);
 }
 
 // Adding new expense Cash in Hand
-function AddExpense(expense) {
+function AddExpense(context,expense) {
 
 }
 
@@ -50,22 +77,22 @@ function endTrip() {
 
 // Functions declared below are required 
 function EventHandler(context, event) {
-    if(! context.simpledb.botleveldata.numinstance)
+    if (!context.simpledb.botleveldata.numinstance)
         context.simpledb.botleveldata.numinstance = 0;
     numinstances = parseInt(context.simpledb.botleveldata.numinstance) + 1;
     context.simpledb.botleveldata.numinstance = numinstances;
-    context.sendResponse(Thanks for adding me. You are + numinstances);
+    context.sendResponse("Thanks for adding me. You are" + numinstances);
 }
 
 function HttpResponseHandler(context, event) {
-     if(event.geturl === httpip-api.comjson)
+    //    if (event.geturl === httpip - api.comjson)
     context.sendResponse(event.getresp);
 }
 
 function DbGetHandler(context, event) {
-    context.sendResponse(testdbput keyword was last get by + event.dbval);
+    context.sendResponse("testdbput keyword was last get by" + event.dbval);
 }
 
 function DbPutHandler(context, event) {
-    context.sendResponse(testdbput keyword was last put by + event.dbval);
+    context.sendResponse("testdbput keyword was last put by" + event.dbval);
 }
